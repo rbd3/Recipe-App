02e884c8927da819384a4ea3af48f2e1be19da6d
rbd3 <rabedesanaandry01@gmail.com>
2023-10-25 11:49:54 +0300

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,17 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'recipe_foods/index'
-  get 'recipe_foods/show'
-  get 'recipe_foods/new'
-  get 'recipe_foods/create'
-  get 'recipe_foods/update'
-  get 'recipe_foods/destroy'
-  get 'recipes/index'
-  get 'recipes/show'
-  get 'recipes/new'
-  get 'recipes/create'
-  get 'recipes/update'
-  get 'recipes/destroy'
-  get 'recipe/index'
-  get 'recipe/show'
-  get 'recipe/new'
-  get 'recipe/update'
-  get 'recipe/destroy'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users, only: [:index, :show, :new, :create, :destroy] do
+    resources :foods, [:index, :show, :new, :create, :destroy]
+    resources :recipes, [:index, :show, :new, :create, :destroy]
+  end
+
+  resources :recipes do
+    resources :recipe_foods, [:index, :show, :new, :create, :destroy]
+  end
 end
